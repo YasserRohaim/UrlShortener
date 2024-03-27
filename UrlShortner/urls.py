e6223ac5ac9d0a3e16b772156b5c8560fc12c28d
YasserRohaim <yasser.rohaim9@gmail.com>
2024-03-27 22:13:37 +0200

    minor fix

--- UrlShortner/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path, include
-from main import urls
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("",include(urls))
-]
